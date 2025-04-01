ae98cf5acf7f39228dea0578e1cbf430efd131c5
azavgo <azavgo@192-168-1-105.tpgi.com.au>
2025-04-01 19:07:40 +1100

    Changing the theme to dracula

--- lua/plugins/dracula.lua
+++ lua/plugins/dracula.lua
@@ -1,6 +1,19 @@
 return { 
-	"Mofiqul/dracula.nvim",
-    config = function() 
-                vim.cmd.colorscheme "dracula";
-             end
+  "binhtran432k/dracula.nvim",  
+  opts = {},
+  keys = {
+            { 
+             "<leader>ds", 
+             function() vim.cmd.colorscheme "dracula-soft" end, 
+             desc = "Switches the Neovim theme to dracula-soft"
+            },
+            { 
+             "<leader>dl", 
+             function() vim.cmd.colorscheme "dracula-day" end, 
+             desc = "Switches the Neovim theme to dracula-day"
+            }
+        },
+  config = function() 
+             vim.cmd.colorscheme "dracula-soft"
+           end
 }
