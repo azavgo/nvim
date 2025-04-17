8b6a968ce79a1d5a727301884b73e7db80978604
azavgo <azavgo@argon.local>
2025-04-17 10:57:04 +1000

    Added LSP support

--- lua/config/shortcuts.lua
+++ lua/config/shortcuts.lua
@@ -1,1 +1,2 @@
 vim.keymap.set('n', '-', "<cmd>Oil --float<CR>", {desc = "Loads oil file manager"})
+vim.keymap.set('n', "<leader>cf", function() require("conform").format() end, {desc = "Formats the code"})
